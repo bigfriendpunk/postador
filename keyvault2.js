c4e76d33ef3adec7ddbd93e0561fe51b18e0026c
bigfriendpunk <bigfriendpunk@hotmail.com>
2023-07-04 23:56:35 -0300

    Create keyvault2.js

---
+++ keyvault2.js
@@ -0,0 +1,80 @@
+const { SecretClient } = require("@azure/keyvault-secrets");
+const { DefaultAzureCredential } = require("@azure/identity");
+const fastify = require('fastify')({ logger: true });
+const path = require('path');
+
+const options = {
+  port: 3000,
+  host: 'localhost'
+};
+
+class KeyVaultService {
+  constructor(vaultUrl) {
+    this.vaultUrl = vaultUrl;
+    this.credential = new DefaultAzureCredential();
+    this.client = new SecretClient(this.vaultUrl, this.credential);
+  }
+
+  async importSecretToKeyVault(secretName, secretValue) {
+    try {
+      const result = await this.client.setSecret(secretName, secretValue);
+      console.log(`The secret ${result.name} was created.`);
+      return result;
+    } catch (err) {
+      console.error("Error creating secret:", err);
+      throw err;
+    }
+  }
+
+  validateFileExtension(filename) {
+    const validExtensions = ['.pem', '.key'];
+    const ext = path.extname(filename);
+    
+    if (!validExtensions.includes(ext)) {
+      throw new Error(`Invalid file extension. Only .pem or .key files are allowed.`);
+    }
+  }
+
+  async uploadAndImportSecrets(parts) {
+    for await (const part of parts) {
+      if (part.filename) {
+        this.validateFileExtension(part.filename);
+
+        const ext = path.extname(part.filename);
+        const secretName = path.basename(part.filename, ext);
+        const secretValue = part.file.toString('utf8');
+
+        await this.importSecretToKeyVault(secretName, secretValue);
+      }
+    }
+
+    return { message: 'secrets created' };
+  }
+}
+
+const vaultUrl = 'https://marvintst.vault.azure.net';
+const keyVaultService = new KeyVaultService(vaultUrl);
+
+fastify.register(require('@fastify/multipart'));
+
+fastify.post('/upload', async (request, reply) => {
+  const parts = request.parts();
+
+  try {
+    await keyVaultService.uploadAndImportSecrets(parts);
+    console.error('secrets created');
+    reply.code(201).send({ message: 'secrets created' });
+  } catch (err) {
+    console.error('Error uploading secrets:', err);
+    reply.code(500).send({ message: 'error uploading secrets' });
+  }
+});
+
+fastify.listen(options, (err, address) => {
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
+
+  console.log(`Server listening on ${address}`);
+});
